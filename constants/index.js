234e7b8442b9a1c8193929b72592733ae45e97a5
ruthlessbanana <150960508+ruthlessbanana@users.noreply.github.com>
2024-04-26 15:57:20 +0000

    first commit

--- constants/index.js
+++ constants/index.js
@@ -27,9 +27,9 @@ export const exploreWorlds = [
 ];
 
 export const startingFeatures = [
-  'Find a world that suits you and you want to enter',
-  'Enter the world by reading basmalah to be safe',
-  'No need to beat around the bush, just stay on the gas and have fun',
+  'Join our Telegram',
+  'Acquire $ZING to play',
+  'Type "roll" and have fun',
 ];
 
 export const newFeatures = [
