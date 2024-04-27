2f2ce031f1ec9e0b7307317843406b34925fe54a
ruthlessbanana <150960508+ruthlessbanana@users.noreply.github.com>
2024-04-27 20:10:15 +0000

    updates

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { motion } from 'framer-motion';
-
 import styles from '../styles';
 import { navVariants } from '../utils/motion';
 
@@ -14,9 +13,9 @@ const Navbar = () => (
   >
     <div className="absolute w-[50%] inset-0 gradient-01" />
     <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
-      <img src="/search.svg" alt="search" className="w-[24px] h-[24px] object-contain"/>
-      <img src="/zinglogo.png" alt="search" className="w-[96px] h-[48px] object-fill"/>
-      <img src="/menu.svg" alt="menu" className="w-[24px] h-[24px] object-contain"/>
+      <img src="/search.svg" alt="search" className="w-[24px] h-[24px] object-contain" />
+      <img src="/zinglogo.png" alt="search" className="w-[96px] h-[48px] object-fill" />
+      <img src="/menu.svg" alt="menu" className="w-[24px] h-[24px] object-contain" />
     </div>
   </motion.nav>
 );
